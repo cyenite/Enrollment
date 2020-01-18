efee88f69fe0f0fc27173d539067f4d6613ef693
Akron254 <aaronrono42@gmail.com>
2020-01-18 23:03:48 +0300

    URL Variables

--- routes.py
+++ routes.py
@@ -11,10 +11,15 @@ def login():
     return render_template("login.html",login=True)
 
 @app.route("/courses")
-def courses():
-    courseData = [{"courseID":"1111","title":"PHP 101","description":"Intro to PHP","credits":3,"term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":4,"term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":3,"term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":3,"term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":4,"term":"Fall"}]
+@app.route("/courses <term>")
+def courses(term = "Spring 2020"):
+    courseData = [{"courseID":"1111","title":"PHP 101","description":"Intro to PHP","credits":3,"term":"Fall, Spring"},
+     {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":4,"term":"Spring"},
+      {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":3,"term":"Fall"},
+       {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":3,"term":"Fall, Spring"},
+        {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":4,"term":"Fall"}]
 
-    return render_template("courses.html",login=True, courseData = courseData)
+    return render_template("courses.html",login=True, courseData = courseData, term=term)
 
 @app.route("/register")
 def register():
